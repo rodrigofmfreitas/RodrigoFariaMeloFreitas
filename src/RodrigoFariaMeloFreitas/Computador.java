7aa6710fbe207f4dfde99525ada79fb78e42220f
Rodrigo Melo <digofmf@gmail.com>
2023-05-23 00:37:08 -0300

    .

--- src/RodrigoFariaMeloFreitas/Computador.java
+++ src/RodrigoFariaMeloFreitas/Computador.java
@@ -1,5 +1,9 @@
 package RodrigoFariaMeloFreitas;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Computador {
     private String[] marcas = {"Dell", "Lenovo", "HP", "Positivo", "Asus", "Apple", "IBM"};
     private String[] processadores = {"Intel Core i3", "Intel Core i5", "Intel Core i7",
@@ -7,8 +11,8 @@ public class Computador {
     private int[] tamanhoTelas = {10, 12, 15, 20, 25, 28};
 
 
-    private String codComp;
     private char ativo;
+    private String codComp;
     private String marca;
     private String modelo;
     private String processador;
@@ -44,6 +48,106 @@ public class Computador {
             }
         }
         return false;
+    }
+
+    public long searchComputer(String compSearch) {
+        long cursorLocation = 0;
+        try {
+            RandomAccessFile computerFile = new RandomAccessFile("COMP.DAT", "rw");
+            while (true) {
+                cursorLocation  = computerFile.getFilePointer();
+                ativo           = computerFile.readChar();
+                codComp         = computerFile.readUTF();
+                marca           = computerFile.readUTF();
+                modelo          = computerFile.readUTF();
+                processador     = computerFile.readUTF();
+                quantMemoria    = computerFile.readInt();
+                tamanhoTela     = computerFile.readInt();
+                quantEstoque    = computerFile.readInt();
+                preco           = computerFile.readFloat();
+                quantVendida    = computerFile.readInt();
+                dtUltimaVenda   = computerFile.readUTF();
+
+                if (compSearch.equals(codComp) && ativo == 'S') {
+                    computerFile.close();
+                    return cursorLocation;
+                }
+            }
+        } catch (EOFException e) {
+            return -1;
+        } catch (IOException e) {
+            System.out.println("Erro na abertura do arquivo - programa sera finalizado");
+            System.exit(0);
+            return -1;
+        }
+    }
+
+    public String findLastOCcurrence (String compBrand) {
+        long cursorLocation = 0;
+        String lastOccurrence = "no";
+        try {
+            RandomAccessFile computerFile = new RandomAccessFile("COMP.DAT", "rw");
+            while (true) {
+                cursorLocation  = computerFile.getFilePointer();
+                ativo           = computerFile.readChar();
+                codComp         = computerFile.readUTF();
+                marca           = computerFile.readUTF();
+                modelo          = computerFile.readUTF();
+                processador     = computerFile.readUTF();
+                quantMemoria    = computerFile.readInt();
+                tamanhoTela     = computerFile.readInt();
+                quantEstoque    = computerFile.readInt();
+                preco           = computerFile.readFloat();
+                quantVendida    = computerFile.readInt();
+                dtUltimaVenda   = computerFile.readUTF();
+
+                if (compBrand.equals(codComp.substring(0, 2)) && ativo == 'S') {
+                    lastOccurrence = codComp;
+                }
+            }
+        } catch (EOFException e) {
+            return lastOccurrence;
+        } catch (IOException e) {
+            System.out.println("Erro na abertura do arquivo - programa sera finalizado");
+            System.exit(0);
+            return "error";
+        }
+    }
+
+    public void saveComputer () {
+        try {
+            RandomAccessFile computerFile = new RandomAccessFile("COMP.DAT", "rw");
+
+            computerFile.seek(computerFile.length());
+            computerFile.writeChar(ativo);
+            computerFile.writeUTF(codComp);
+            computerFile.writeUTF(marca);
+            computerFile.writeUTF(modelo);
+            computerFile.writeUTF(processador);
+            computerFile.writeInt(quantMemoria);
+            computerFile.writeInt(quantEstoque);
+            computerFile.writeFloat(preco);
+            computerFile.writeInt(quantVendida);
+            computerFile.writeUTF(dtUltimaVenda);
+            computerFile.close();
+            System.out.println("Dados gravados com sucesso!\n");
+        } catch (IOException e) {
+            System.out.println("Erro na abertura do arquivo - programa sera finalizado");
+            System.exit(0);
+        }
+    }
+
+    public void deactivateComputer (long pos) {
+        try {
+            RandomAccessFile computerFile = new RandomAccessFile("COMP.DAT", "rw");
+
+            computerFile.seek(pos);
+            computerFile.writeChar('N');
+            computerFile.close();
+        } catch (IOException e) {
+            System.out.println("Erro na abertura do arquivo  -  programa sera finalizado");
+            System.exit(0);
+        }
     }
 
     public void incluir() {
@@ -99,5 +203,43 @@ public class Computador {
         ativo = 'S';
         quantVendida = 0;
         dtUltimaVenda = "01/01/2000";
+
+        if (marca.equals("Dell")) {
+            codComp = generateCodComp("DE");
+        } else if (marca.equals("Lenovo")) {
+            codComp = generateCodComp("LE");
+        } else if (marca.equals("HP")) {
+            codComp = generateCodComp("HP");
+        } else if (marca.equals("Positivo")) {
+            codComp = generateCodComp("PO");
+        } else if (marca.equals("Asus")) {
+            codComp = generateCodComp("AS");
+        } else if (marca.equals("Apple")) {
+            codComp = generateCodComp("AP");
+        } else {
+            codComp = generateCodComp("IB");
+        }
+
+    }
+
+    public String generateCodComp (String compBrand) {
+        String temp;
+        int serialNum;
+        temp = findLastOCcurrence(compBrand);
+        if (temp.equals("no")) {
+            temp = compBrand + "0001";
+        } else {
+            serialNum = Integer.parseInt(temp.substring(2));
+            if (serialNum < 10) {
+                temp = compBrand + "000" + Integer.toString(serialNum);
+            } else if (serialNum < 100) {
+                temp = compBrand + "00" + Integer.toString(serialNum);
+            } else if (serialNum < 1000) {
+                temp = compBrand + "0" + Integer.toString(serialNum);
+            } else {
+                temp = compBrand + Integer.toString(serialNum);
+            }
+        }
+        return temp;
     }
 }
